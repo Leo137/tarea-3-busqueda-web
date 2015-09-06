66eadde696fce2b2256b5d0ac3c86cfcd8c103ac
Leo137 <leo.psicokinesis@gmail.com>
2015-09-06 19:15:50 -0400

    First commit

---
+++ tarea3.py
@@ -0,0 +1,9 @@
+# @ Tarea 3 - Tecnologias de busqueda en la Web
+#             Leonardo Santis - 2010735478 - leonardo.santis@alumnos.usm.cl
+#             Erick Sepulveda - 201073501-k - erick.sepulveda@alumnos.usm.cl
+#             Milenko Castillo - rol - mail
+
+import os
+import re
+import math
+import time
